0afd7204bc43a9d2241bb46a549dce0278575cbe
aymensellaouti <aymen.sellaouti@gmail.com>
2026-02-06 15:25:10 +0100

    enonce exo sensors

--- sensors.js
+++ sensors.js
@@ -1,3 +1,22 @@
+/*
+Vous travaillez sur le système de monitoring d'une ville connectée. 
+Vous recevez un tableau d'objets représentant les capteurs déployés dans la ville (température, pollution, humidité).1. 
+const sensors = [  
+        { id: "TEMP_01", type: "temperature", value: 22.5, status: "active" },  
+        { id: "POLL_01", type: "pollution", value: 85, status: "active" }, 
+        { id: "HUM_01", type: "humidity", value: 45, status: "inactive" },  
+        { id: "TEMP_02", type: "temperature", value: 31.0, status: "active" },  
+        { id: "POLL_02", type: "pollution", value: 42, status: "active" },  
+        { id: "TEMP_03", type: "temperature", value: -5.0, status: "faulty" }]; 
+Parcourir le tableau et afficher dans la console l'ID de chaque capteur et son état (ex: "Capteur TEMP_01 : active").
+Créer un nouveau tableau criticalAlerts. Un capteur doit y être ajouté si :Son type est "pollution" et sa valeur est > 80.OU son statut est "faulty".
+Affichez le tableau final.
+Calculer la température moyenne de tous les capteurs de type "temperature" (uniquement ceux qui ne sont pas "faulty").
+Créer un script qui pour un type de capteur, change le statut de tous les capteurs de ce type de "inactive" ou "faulty" vers "active". 
+
+
+
+*/
 const sensors = [
     { id: "TEMP_01", type: "temperature", value: 22.5, status: "active" },
     { id: "POLL_01", type: "pollution", value: 85, status: "active" },
